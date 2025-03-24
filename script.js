a833d2a96d2c299e3ea92ad9d0ca332badbb5061
hlain97 <halimnakad97@gmail.com>
2025-03-24 01:10:05 -0300

    Added board

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("aaa")
+const gameboard = {
+    rowOne: ["X", "O", "O"],
+    rowTwo: ["O", "X", "O"],
+    rowThree: ["O", "O", "X"],
+}
+
+function checker(object){
+    for(e in object) {
+        console.log(`${object[e]}`);
+    }
+}
+
+checker(gameboard);
