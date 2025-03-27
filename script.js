e5416d788a72a2871275229584b2ba9437916b03
hlain97 <halimnakad97@gmail.com>
2025-03-26 22:18:46 -0300

    Making progress

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-const gameboard = {
-    rowOne: ["X", "O", "O"],
-    rowTwo: ["O", "X", "O"],
-    rowThree: ["O", "O", "X"],
-}
+let gameBoardDom = document.getElementById("gameboard");
 
-function checker(object){
-    for(e in object) {
-        console.log(`${object[e]}`);
+const gameboard = {
+    rowOne: ["", "", ""],
+    rowTwo: ["", "", ""],
+    rowThree: ["", "", ""],
+};
+
+let turn = false;
+
+let checker = function(board){
+    for(const row in board){
+        board[row].forEach(cell =>{
+            const markBox = () => {
+                if(turn == false){
+                    box.textContent = 'X'
+                    turn = true;
+                    box.removeEventListener('click', this);
+                } else if (turn == true){
+                    box.textContent = 'O'
+                    turn = false;
+                }
+            box.removeEventListener('click', markBox);
+            }
+            let box = document.createElement('div')
+            box.classList.toggle('block')
+            box.addEventListener('click', markBox)
+            gameBoardDom.appendChild(box);
+        })
     }
 }
-
 checker(gameboard);
